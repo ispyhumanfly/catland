733352961b08aaa184f966a7e5b1fda1334981fc
Dan Stephenson <ispyhumanfly@gmail.com>
2020-04-26 17:24:44 -0400

    I think I have basic uploads working.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
 from flask import Flask
 app = Flask(__name__, static_url_path="/", static_folder="/assets")
+app.config['UPLOAD_FOLDER'] = "."
+
 from app import routes 
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
 
 import boto3
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 @app.route('/index')
@@ -22,7 +28,7 @@ def about():
     return render_template("about.html", message=message)
 
 @app.route('/gallery')
-def uploader():
+def gallery():
     message = "Gallery"
     return render_template('gallery.html', message=message)
 
@@ -33,7 +39,22 @@ def uploader():
     if request.method == 'GET':
         return render_template('uploader.html', message=message)
     if request.method == 'POST':
-        return 
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+        
 
 @app.route('/db')
 def db():
