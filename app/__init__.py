7c135a94bafed6b7704c85ee259bb7a25515a7ed
Dan Stephenson <ispyhumanfly@gmail.com>
2020-04-28 19:20:44 -0400

    Updated static assets path.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-app = Flask(__name__, static_url_path="/", static_folder="/assets")
+app = Flask(__name__, static_url_path="/", static_folder="./assets")
 
 app.config['UPLOAD_FOLDER'] = './uploads'
 app.config['SECRET_KEY'] = 'the random string'    
